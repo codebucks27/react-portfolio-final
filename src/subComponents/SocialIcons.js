cd5b632fc6af48242b77241ea9b40df7a9dade7c
CodeBucks <codebucks27@gmail.com>
2022-12-12 16:13:41 +0530

    navlink changes

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import React from "react";
-import { NavLink } from "react-router-dom";
+// import { a } from 'react-router-dom'
 import styled from "styled-components";
 import { Facebook, Github, Twitter, YouTube } from "../components/AllSvgs";
 import { DarkTheme } from "../components/Themes";
@@ -31,74 +31,73 @@ const Line = styled(motion.span)`
 const SocialIcons = (props) => {
   return (
     <Icons>
-    {/* changed initial [transform: "scale(0)"] to [scale: 0] */}
       <motion.div
-        initial={{ scale: 0 }}
+        initial={{ transform: "scale(0)" }}
         animate={{ scale: [0, 1, 1.5, 1] }}
         transition={{ type: "spring", duration: 1, delay: 1 }}
       >
-        <NavLink
+        <a
           style={{ color: "inherit" }}
           target="_blank"
-          to={{ pathname: "https://github.com/codebucks27" }}
+          href={"https://github.com/codebucks27"}
         >
           <Github
             width={25}
             height={25}
             fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
           />
-        </NavLink>
+        </a>
       </motion.div>
       <motion.div
-        initial={{ scale: 0 }}
+        initial={{ transform: "scale(0)" }}
         animate={{ scale: [0, 1, 1.5, 1] }}
         transition={{ type: "spring", duration: 1, delay: 1.2 }}
       >
-        <NavLink
+        <a
           style={{ color: "inherit" }}
           target="_blank"
-          to={{ pathname: "https://twitter.com/code_bucks" }}
+          href={"https://twitter.com/code_bucks"}
         >
           <Twitter
             width={25}
             height={25}
             fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
           />
-        </NavLink>
+        </a>
       </motion.div>
       <motion.div
-        initial={{ scale: 0 }}
+        initial={{ transform: "scale(0)" }}
         animate={{ scale: [0, 1, 1.5, 1] }}
         transition={{ type: "spring", duration: 1, delay: 1.4 }}
       >
-        <NavLink
+        <a
           style={{ color: "inherit" }}
           target="_blank"
-          to={{ pathname: "https://facebook.com/codebucks27" }}
+          href={"https://facebook.com/codebucks27"}
         >
           <Facebook
             width={25}
             height={25}
             fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
           />
-        </NavLink>
+        </a>
       </motion.div>
       <motion.div
-        initial={{ scale: 0 }}
+        initial={{ transform: "scale(0)" }}
         animate={{ scale: [0, 1, 1.5, 1] }}
         transition={{ type: "spring", duration: 1, delay: 1.6 }}
       >
-        <NavLink
+        <a
           style={{ color: "inherit" }}
           target="_blank"
-          to={{ pathname: "https://youtube.com" }}
+          href={"https://youtube.com"}
         >
           <YouTube
             width={25}
             height={25}
             fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
           />
-        </NavLink>
+        </a>
       </motion.div>
 
       <Line
