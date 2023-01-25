aed0ee31b7f66430f0aed6d7ec97dd2299a4a303
CodeBucks <codebucks27@gmail.com>
2023-01-25 19:14:29 +0530

    Fixed blog links

--- src/components/BlogComponent.js
+++ src/components/BlogComponent.js
@@ -1,10 +1,10 @@
 import { motion } from 'framer-motion'
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+// import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
 
-const Box = styled(motion(NavLink))`
+const Box = styled(motion.a)`
 width: calc(10rem + 15vw);
 text-decoration: none;
 height: 20rem;
